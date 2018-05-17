39a5ee6394d3326fcb1e19c5b65869533944b2f2
hippieZhou <hippiezhou@outlook.com>
2018-05-17 22:14:29 +0800

    init

---
+++ 03.Sample/DriverCommands.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Sample
+{
+    class DriverCommands
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.demoqa.com";
+
+            string title = driver.Title;
+            Console.WriteLine($"Title of the page is:{title}");
+            int titleLength = driver.Title.Length;
+            Console.WriteLine($"Length of the title is:{titleLength}");
+            string PageURL = driver.Url;
+            Console.WriteLine($"URL of the page is : {PageURL}");
+            int URLLength = driver.Url.Length;
+            Console.WriteLine($"Length of the URL is : {URLLength}");
+            string PageSource = driver.PageSource;
+            int PageSourceLength = driver.PageSource.Length;
+            Console.WriteLine($"Page Source of the page is : {PageSource}");
+            Console.WriteLine($"Length of the Page Source is : {PageSourceLength}");
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test02()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://demoqa.com/frames-and-windows/";
+            driver.FindElement(By.XPath(".//*[@id='tabs-1']/div/p/a")).Click();
+            //driver.Close();
+        }
+    }
+}
---
+++ 03.Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Sample
+{
+    /// <summary>
+    /// http://toolsqa.com/selenium-webdriver/c-sharp/iwebdriver-browser-commands-in-c-sharp/
+    /// webdriver-browser-commands-in-c-sharp
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
