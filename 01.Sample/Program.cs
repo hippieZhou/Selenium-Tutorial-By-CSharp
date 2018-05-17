15ed6aa0b369cd5bd4163963a5745f9b6571305b
hippieZhou <hippiezhou@outlook.com>
2018-05-17 20:54:38 +0800

    init

---
+++ 01.Sample/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Sample
+{
+    /// <summary>
+    /// http://toolsqa.com/selenium-webdriver/c-sharp/set-up-selenium-webdriver-with-visual-studio-in-c/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * FirefoxDriver
+             * ChromeDriver
+             * EdgeDriver
+             * InternetExplorerDriver
+             */
+
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://demoqa.com/";
+
+            
+        }
+    }
+}
---
+++ 02.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Sample
+{
+    /// <summary>
+    /// How to write Selenium Test using NUnit Framework
+    /// http://toolsqa.com/selenium-webdriver/c-sharp/how-to-write-selenium-test-using-nunit-framework/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
