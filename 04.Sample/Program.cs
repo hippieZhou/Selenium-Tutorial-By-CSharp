bfa77814f575413256438f480c3050fa2f27b40f
hippieZhou <hippiezhou@outlook.com>
2018-05-17 22:22:44 +0800

    init

---
+++ 04.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            //driver.Url = "http://www.bing.com";
+            driver.Navigate().GoToUrl("http://demoqa.com");
+            driver.FindElement(By.XPath(".//*[@id='menu-item-374']/a")).Click();
+            driver.Navigate().Back();
+            driver.Navigate().Forward();
+            driver.Navigate().Refresh();
+        }
+    }
+}
