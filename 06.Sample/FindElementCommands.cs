13c009e4b78036f328543407938d6dc158ba4579
hippieZhou <hippiezhou@outlook.com>
2018-05-19 17:35:47 +0800

    init

---
+++ 06.Sample/FindElementCommands.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Sample
+{
+    class FindElementCommands
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.baidu.com";
+            driver.FindElement(By.Id("kw")).SendKeys("hippie");
+            driver.FindElement(By.Id("su")).Click();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.baidu.com";
+            driver.FindElement(By.Id("kw"));
+            Console.WriteLine("Partical like test Pass");
+
+            string sClass = driver.FindElement(By.Name("tj_trnews")).ToString();
+            Console.WriteLine(sClass);
+
+            driver.FindElement(By.LinkText("新闻")).Click();
+        }
+    }
+}
---
+++ 06.Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Sample
+{
+    /// <summary>
+    /// http://toolsqa.com/selenium-webdriver/c-sharp/findelement-and-findelements-commands-in-c/
+    /// FindElement和FindElements命令在C＃中
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+        }
+    }
+}
