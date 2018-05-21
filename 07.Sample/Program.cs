dcbb027f5a5e4958c5f86a55007b252dbd06e18e
hippieZhou <hippiezhou@outlook.com>
2018-05-21 18:10:33 +0800

    init

---
+++ 07.Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Sample
+{
+    /// <summary>
+    /// http://toolsqa.com/selenium-webdriver/c-sharp/checkbox-radio-button-operations-c/
+    /// C＃中的复选框和单选按钮操作
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Radio 
+
+            #endregion
+        }
+    }
+}
